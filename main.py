10e3470fe520230446d7dbc6147185ae66bec79d
Engi <57834634+ChronEngi@users.noreply.github.com>
2024-01-08 23:14:46 +0100

    More stable

--- main.py
+++ main.py
@@ -1,45 +1,46 @@
-import os
-import psutil
-import time
-import subprocess
-import customtkinter as ctk
-import keyboard
+# PyTerminalCraft. ChronEngi project on GitHub.
+# Allows reading Minecraft chat, via reading the Minecraft Java 'latest.log'.
+# It recognizes messages and translates them into terminal commands.
+# Keep in mind that the code will read chat messages from any player. I do NOT recommend using it in multiplayer.
+
+
+
+
+import os                       # Library for accessing system information such as the Minecraft Java path location and execute the command.
+import psutil                   # Library to access tasks and see if Minecraft Java is running.
+import customtkinter as UI      # Library to inilialize a modern graphical user interface.
 
 # Dichiarazioni globali
 new_chat_message = ""
 told_its_open = False
 user_confirmed = False
 personal_path = r'C:\Users\prosp\OneDrive\Minecraft\Dati Minecraft\logs\latest.log'
+mc_status = False
 
 # Configurazione UI
-ctk.set_appearance_mode("Dark")
-ctk.set_default_color_theme("chron_theme.json")
+UI.set_appearance_mode("Dark")
+UI.set_default_color_theme("resources/chron_theme.json")
 
-app = ctk.CTk()
+app = UI.CTk()
 app.geometry('500x300')
 app.title('PyTerminalCraft')
-app.iconbitmap('icon.ico')
+app.iconbitmap('resources/icon.ico')
 app.resizable(False, False)
 
 if os.path.exists(personal_path):
     default_mc_path = personal_path
 else:
-    default_mc_path = os.path.join(os.environ['APPDATA'], 'Roaming', '.minecraft')
+    default_mc_path = os.path.join(os.environ['APPDATA'], 'Roaming', r'.minecraft\logs\latest.log')
     
 print("The path is:", default_mc_path)
 
 # Funzioni
 def confirm():
     global user_confirmed
-    print('User confirmed')
+    print('\nUser confirmed!\n')
     user_confirmed = True
-
-    # Nascondi la finestra e contenuti
-    #app.iconify()
-    app.geometry('0x0')
     app.overrideredirect(True)
-    #log_dir.forget()
-    #confirm_but.forget()
+    app.geometry('0x0')
 
 
 def read_log():
@@ -55,16 +56,20 @@ def read_log():
         return None
 
 def is_minecraft_running():
+    global mc_status
     for process in psutil.process_iter(['pid', 'name', 'cmdline']):
         if 'java' in process.info['name'].lower() and 'minecraft' in ' '.join(process.info['cmdline']).lower():
+            mc_status = True
             return True
+    mc_status = False
     return False
 
 def read_mc_chat():
     global new_chat_message
     global told_its_open
 
-    if is_minecraft_running():
+    # if is_minecraft_running():
+    if mc_status is True:
 
         if told_its_open is False:
             print('Minecraft docked!')
@@ -80,47 +85,36 @@ def read_mc_chat():
 
                 print('Command: ' + only_text)
 
-                # Esegui il comando usando subprocess
-                subprocess.run(only_text, shell=True)
-
-                # os.system(only_text)
-
-                # if only_text == "notepad":
-                #     os.system('notepad.exe')
-
+                # Esegui il comando usando
+                os.system(only_text)
 
     # Richiama nuovamente la funzione dopo un certo periodo
-    app.after(200, read_mc_chat)
+    app.after(1000, read_mc_chat)
 
 # Elementi UI
-log_dir = ctk.CTkEntry(app, width=450)
-log_dir.pack(padx=10, pady=10)
+log_dir_title = UI.CTkLabel(app, text='Minecraft log path')
+log_dir_title.pack(padx=10, pady=3)
+
+log_dir = UI.CTkEntry(app, width=450)
+log_dir.pack(padx=10, pady=3)
 log_dir.insert(0, default_mc_path)
 
-confirm_but = ctk.CTkButton(app, text='Confirm', width=30, command=confirm)
-confirm_but.pack(padx=10, pady=10)
+delay_value_title = UI.CTkLabel(app, text='Check every (ms)')
+delay_value_title.pack(padx=10, pady=3)
 
+delay_value = UI.CTkEntry(app, width=150)
+delay_value.pack(padx=10, pady=3)
+delay_value.insert(0, 1000)
 
-# Avvia la funzione di lettura nel thread principale di tkinter
+confirm_but = UI.CTkButton(app, text='Confirm', width=30, command=confirm)
+confirm_but.pack(padx=10, pady=40)
+
+git_credits = UI.CTkLabel(app, text='By ChronEngi on GitHub')
+git_credits.place(relx=0.5, rely=1.0, anchor='s')
+
+is_minecraft_running()
+# Inizia il loop di tkinter
+# if user_confirmed is True:
 read_mc_chat()
 
-# Function to prevent window from being closed
-def on_closing():
-    print("Window close button clicked, but preventing window from closing.")
-
-# Bind the protocol method to the on_closing function
-#app.protocol("WM_DELETE_WINDOW", on_closing)
-
-# Funzione chiamata quando viene premuta la combinazione di tasti di emergenza
-def emergency_hotkey():
-    app.geometry('500x300')
-    app.deiconify()
-    app.overrideredirect(False)
-    app.lift()
-    app.focus_force()
-
-# Show Window
-keyboard.add_hotkey('altgr+l', emergency_hotkey)
-
-# Inizia il loop di tkinter
 app.mainloop()
