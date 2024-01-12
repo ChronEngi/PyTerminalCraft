ef77250ca8ec64c4712a0ca1cd12f7b0a00c885a
Engi <57834634+ChronEngi@users.noreply.github.com>
2024-01-12 20:49:04 +0100

    Better 'run.bat' and 'setup.bat'

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
+cls
 python main.py
--- setup.bat
+++ setup.bat
@@ -1,3 +1,17 @@
+@echo off
+cls
+echo Downloading Python...
+
 winget install Python
+
+cls
+echo Downloading libaries
+
 pip3 install tk
 pip3 install customtkinter
+cls
+color 3
+
+echo Everything should be ready!
+
+pause
